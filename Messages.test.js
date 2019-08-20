63bd8751f0aa314dccd411269eb7cdcb742efeba
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-19 23:51:45 -0700

    Updated the testing file for the Snapshot tests
    changed exportAllDeclaration to export(tree) on lines 17, and 24.

--- Messages.test.js
+++ Messages.test.js
@@ -8,18 +8,18 @@ describe('Messages component', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Messages />, div);
         ReactDOM.unmountComponentAtNode(div);
-    })
+    });
     
     it('renders the UI as expected', () => {
         const tree = renderer
             .create(<Messages name="Messages" unread={4}/>)
             .toJSON();
-        exportAllDeclaration(tree).toMatchSnapshot();
+        expect(tree).toMatchSnapshot();
     });
     
     it('renders the UI as expected with no unreads', () => {
         const tree = renderer
-            .create(<Messages name="Messages" unread={0} />)
+            .create(<Messages name="Messages" unread={0}/>)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
