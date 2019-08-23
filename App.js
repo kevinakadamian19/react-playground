9840e4a7837ff97240d8968785f702dda232b294
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-23 16:36:30 -0700

    Accordian Component
    Updated App component for Accordian component excercise. Included data to update form sectionsData.

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
 import './App.css';
 import Messages from './Messages'
+import Tabs from './states/Tabs';
+import Accordian from './state-drills/Accordian';
+import sectionsData from './state-drills/sectionsData';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    //I had put the sections array into a separate file, and then set the sections as the current state.
+    this.state = {
+      sections: sectionsData
+    }
+  }
+
+  
+
   render() {
+    //The below is trying to have the sections title, and content create a separate LI for each section.
+    const sectionItems = sectionsData.map(items => <Accordian title={items.title} content={items.content}/>)
+
     return(
       <div>
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0}/>
-        <Messages name="Notifications" unread={10}/>
+        {sectionItems}
       </div>
-    );
+    )
   }
 }
 
